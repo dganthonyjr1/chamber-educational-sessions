7a243e4e1f46eef3da6425e1d715d0de397b2f56
Don Anthony <don@suddenimpactagency.io>
2026-01-28 08:19:39 -0500

    NUCLEAR OPTION: Renamed Home to Landing to bypass server cache

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ import NotFound from "@/pages/NotFound";
 import { Route, Switch } from "wouter";
 import ErrorBoundary from "./components/ErrorBoundary";
 import { ThemeProvider } from "./contexts/ThemeContext";
-import Home from "./pages/Home";
+import Landing from "./pages/Landing";
 import Admin from "./pages/Admin";
 import Course from "./pages/Course";
 import Quiz from "./pages/Quiz";
